02e07f3ccbcb6d3bb9fb2da97c17d833e03d15ab
phiamo <phiamo@googlemail.com>
2012-05-30 00:13:56 +0200

    Updated

---
+++ src/Mopa/Bundle/BooksyncBootstrapBundle/MopaBooksyncBootstrapBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mopa\Bundle\BooksyncBootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MopaBooksyncBootstrapBundle extends Bundle
+{
+
+	public function getParent()
+	{
+		return 'FOSUserBundle';
+	}
+}
