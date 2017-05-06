85b3946e7f22e800230fd69c16714b2da22c9908
phiamo <phiamo@googlemail.com>
2017-05-06 16:20:54 +0200

    allow further auth handling

--- WebSocket/Server/Bridge.php
+++ WebSocket/Server/Bridge.php
@@ -2,8 +2,10 @@
 namespace Mopa\Bundle\FeedBundle\WebSocket\Server;
 
 use Mopa\Bundle\FeedBundle\WebSocket\ConnectionEvents;
+use P2\Bundle\RatchetBundle\WebSocket\Connection\ConnectionInterface;
 use P2\Bundle\RatchetBundle\WebSocket\Connection\ConnectionManagerInterface;
 use P2\Bundle\RatchetBundle\WebSocket\ConnectionEvent;
+use P2\Bundle\RatchetBundle\WebSocket\Payload;
 use Psr\Log\LoggerInterface;
 use Ratchet\ConnectionInterface as SocketConnection;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -33,12 +35,23 @@ class Bridge extends \P2\Bundle\RatchetBundle\WebSocket\Server\Bridge
     }
 
     /**
+     * Handles the connection authentication.
+     *
+     * @param ConnectionInterface $connection
+     * @param Payload $payload
+     */
+    protected function handleAuthentication(ConnectionInterface $connection, Payload $payload)
+    {
+        parent::handleAuthentication($connection, $payload);
+        $this->eventDispatcher->dispatch(ConnectionEvent::SOCKET_AUTH_REQUEST, new ConnectionEvent($connection));
+    }
+
+    /**
      * @param SocketConnection $conn
      * @throws \RuntimeException
      */
     public function onClose(SocketConnection $conn)
     {
-
         if($this->connectionManager->hasConnection($conn)) {
             $connection = $this->connectionManager->closeConnection($conn);
 
